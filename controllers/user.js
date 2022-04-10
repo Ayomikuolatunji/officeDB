4665a5f923fd9a415d0584a7fc186cb7edebb57c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-10 21:49:10 +0100

    regitration api

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,16 @@ const User=require("../models/user")
 const { validationResult }=require("express-validator")
 
 const registration=async(req,res,next)=>{
+   const errors = validationResult(req);
+   try {
     const username=req.body.username
     const email=req.body.email
     const password=req.body.password
+    if(!errors.isEmpty()){
+        const error=new Error('Validation failed')
+        error.status=422
+        throw error
+    }
     console.log(req.body)
     // if email already exist throw erro to the client
     const checkUserExits=await User.findOne({email:email})
@@ -14,13 +21,17 @@ const registration=async(req,res,next)=>{
         error.statusCode=422;
         throw error;
     }
+    const hashedPassword=await bcrypt.hash(password,20)
     const user=await User({
         username,
         email,
-        password
+        hashedPassword
     })
     const userData=await user.save()
     res.status(210).json({message:"Account created successfully"})
+   } catch (error) {
+       
+   }
 }
 
 module.exports=registration

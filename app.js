0c01b45cfdec2cf815eab0f432a3c310d0bd9fea
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-10 09:43:40 +0100

    server setup completed

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+// initialise app
+const app=express()
+
+// call dotenv 
+dotenv.config()
+// convert request to json using express middleware
+app.use(express.json())
+
+// enable cors policy
+app.use(cors())
+
+
+// connecting server
+
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`App running locally on ${process.env.PORT}`)
+})

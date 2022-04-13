56e5775eae4d00886c3ec86603e20b678a465b99
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-13 14:49:31 +0100

    chats routes and model

--- controllers/Chat.js
+++ controllers/Chat.js
@@ -1,12 +1,32 @@
-
+const Chat =require("../models/chat")
 
 const fetchChat=async()=>{
 
 }
 
 
-const postChat=async()=>{
+const postChat=async(req,res,next)=>{
+   try {
+    const {chat,from,to}=req.body
 
+    const chatsCreated=await Chat.create({
+        chat:{text:{chat}},
+        users:[from,to],
+        sender:from
+    })
+    if(!chatsCreated){
+        const error=new Error("failed to create message chat")
+        error.statusCode=422
+        throw error
+    }
+    res.status(201).json({message:"succesfully sent",chat:chatsCreated})
+   } catch (error) {
+       if(!error.statusCode){
+           error.statusCode=500
+       }
+       next(error)
+   }
+    
 }
 
 module.exports={postChat,fetchChat}

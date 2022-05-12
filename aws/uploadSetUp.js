77262f49e3decdbf9d87abe0b1802e020e97eb00
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-12 12:53:06 +0100

    aws issue

--- aws/uploadSetUp.js
+++ aws/uploadSetUp.js
@@ -1,11 +1,12 @@
 const aws = require('aws-sdk');
+require("dotenv").config()
 
 
 
 aws.config.update({
-    secretAccessKey: "R838lSMO8uPqSXz7SDvraVql8rhFCdY2BZ+I544r",
-    accessKeyId: "AKIAYEZ2SCIMCPEPOP7Z",
-    region: 'us-east-1' // region of your bucket
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEYID,
+    region: process.env.REGION // region of your bucket
 });
 
 const s3 = new aws.S3();
